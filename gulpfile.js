84ca1b5cbb5ab7dcdf366bd892d013e14a329a5b
Julian6262 <d.baburin62@gmail.com>
2023-07-18 12:53:01 +0300

    Gulp tp

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default();
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
-
-function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    })
-}
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const del = require('del');
+const newer = require('gulp-newer');
+const svgSprite = require('gulp-svg-sprite');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const include = require('gulp-include');
 
 function styles() {
     return src([
         // 'node_modules/magnific-popup/dist/magnific-popup.css',
-        // 'node_modules/slick-carousel/slick/slick.css',
+        // 'node_modules/swiper/swiper-bundle.css',
         'app/scss/style.scss',
         'app/scss/media.scss'
     ])
@@ -35,7 +35,7 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        // 'node_modules/slick-carousel/slick/slick.js',
+        // 'node_modules/swiper/swiper-bundle.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
@@ -44,42 +44,86 @@ function scripts() {
         .pipe(browserSync.stream())
 }
 
+function fonts() {
+    return src('app/fonts/src/*.*')
+        .pipe(fonter({
+            formats: ['woff', 'ttf']
+        }))
+        .pipe(src('app/fonts/*.ttf'))
+        .pipe(ttf2woff2())
+        .pipe(dest('app/fonts'))
+}
+
+function pages() {
+    return src('app/pages/*.html')
+        .pipe(include({
+            includePaths: 'app/components'
+        }))
+        .pipe(dest('app'))
+        .pipe(browserSync.stream())
+}
+
 function images() {
-    return src('app/img/**/*')
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(dest('dist/img'))
+    return src('app/img/src/*.*', '!app/img/src/*.svg')
+        .pipe(newer('app/img'))
+        .pipe(avif({quality: 50}))
+        .pipe(src('app/img/src/*.*'))
+
+        .pipe(newer('app/img'))
+        .pipe(webp())
+        .pipe(src('app/img/src/*.*'))
+
+        .pipe(newer('app/img'))
+        .pipe(imagemin())
+        .pipe(dest('app/img'))
+}
+
+function sprite() {
+    return src('app/img/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: '../sprite.svg',
+                    example: true
+                }
+            }
+        }))
+        .pipe(dest('app/img'))
 }
 
 function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
     watch(['app/scss/**/*.scss'], styles);
+    watch(['app/img/src/*.*'], images);
+    watch(['app/components/*.*', 'app/pages/*.*'], pages);
     watch(['app/js/**/*.js', '!app/js/**/main.min.js'], scripts);
-    watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 function build() {
+    del.sync('dist');
+    images();
     return src([
+        'app/*.html',
         'app/css/style.min.css',
-        'app/fonts/**/*',
         'app/js/main.min.js',
-        'app/*.html'
+        'app/fonts/*.*',
+        'app/img/*.*',
+        '!app/img/*.svg',
+        'app/img/sprite.svg'
     ], {base: 'app'})
         .pipe(dest('dist'))
 }
 
 exports.styles = styles;
 exports.watching = watching;
-exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.build = build;
 exports.images = images;
-exports.default = parallel(browsersync, watching);
+exports.sprite = sprite;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.default = parallel(styles, scripts, images, fonts, pages, watching);
